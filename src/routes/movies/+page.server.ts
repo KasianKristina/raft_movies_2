2b1b3154d0ed5dffa27e4e1d1fe9e64db982ff2a
Kristina Kasian <kristina.kasian@raftds.com>
2025-08-31 19:25:50 +0300

    database

--- src/routes/movies/+page.server.ts
+++ src/routes/movies/+page.server.ts
@@ -1,12 +1,36 @@
-import { moviesData, suggestionsData } from '$lib/mockData';
+import { suggestionsData } from '$lib/mockData';
 import { newMovieSchema } from '$lib/schemas/movie';
 import { fail, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
+import { prisma } from '$lib/server/prisma';
 
 export const load = async () => {
 	const form = await superValidate(zod(newMovieSchema));
 
-	return { form, movies: moviesData, suggestions: suggestionsData.slice(2, 3) };
+	try {
+		const movies = await prisma.movie.findMany();
+
+		const serializableMovies = movies.map((movie) => ({
+			...movie,
+			rating: movie.rating ? Number(movie.rating) : null,
+		}));
+
+		console.log(serializableMovies);
+
+		return {
+			form,
+			movies: serializableMovies,
+			suggestions: suggestionsData.slice(2, 3),
+		};
+	} catch (error) {
+		console.error('Error loading movies:', error);
+		return {
+			form,
+			movies: [],
+			suggestions: suggestionsData.slice(2, 3),
+			error: 'Failed to load movies',
+		};
+	}
 };
 
 export const actions = {
