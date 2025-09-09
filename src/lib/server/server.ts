e2d5a99e9f5c8c3498fa4dec8f2739722ba68b06
Kristina Kasian <kristina.kasian@raftds.com>
2025-09-09 06:44:54 +0300

    auth

---
+++ src/lib/server/server.ts
@@ -0,0 +1,32 @@
+import { lucia } from '$lib/server/auth';
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	const sessionId = event.cookies.get(lucia.sessionCookieName);
+	if (!sessionId) {
+		event.locals.user = null;
+		event.locals.session = null;
+		return resolve(event);
+	}
+
+	const { session, user } = await lucia.validateSession(sessionId);
+
+	if (session && session.fresh) {
+		// обновляем cookie, если нужно
+		const sessionCookie = lucia.createSessionCookie(session.id);
+		event.cookies.set(sessionCookie.name, sessionCookie.value, {
+			path: '/',
+			...sessionCookie.attributes,
+		});
+	} else if (!session) {
+		const blank = lucia.createBlankSessionCookie();
+		event.cookies.set(blank.name, blank.value, {
+			path: '/',
+			...blank.attributes,
+		});
+	}
+
+	event.locals.user = user;
+	event.locals.session = session;
+	return resolve(event);
+};
